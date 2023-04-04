64a26ee8f7f91688a7546bf1553a3d25c6b4ddd0
Garrettqt <102553912+Garrettqt@users.noreply.github.com>
2023-04-04 17:05:46 +0000

    made the app get recipes

--- myApp.py
+++ myApp.py
@@ -1,9 +1,22 @@
 from flask import Flask
 
+import requests
+
+url = "https://tasty.p.rapidapi.com/recipes/list"
+
+querystring = {"from":"0","size":"20","tags":"under_30_minutes"}
+
+headers = {
+	"X-RapidAPI-Key": "7478f14f65msh230d78cb411f25cp1ff3cajsnacc103b463b8",
+	"X-RapidAPI-Host": "tasty.p.rapidapi.com"
+}
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return (response.text)
 
 app.run(host='0.0.0.0', port=5000)
